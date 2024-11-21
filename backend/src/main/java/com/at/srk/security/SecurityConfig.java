92a75d19ca655ff154ebbafedd8ca7db6216d330
Testino Aldo <aldo.testino@olivetti.com>
2024-11-21 11:46:58 +0100

    add imports for decoding

--- backend/src/main/java/com/at/srk/security/SecurityConfig.java
+++ backend/src/main/java/com/at/srk/security/SecurityConfig.java
@@ -11,8 +11,18 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
 import org.springframework.beans.factory.annotation.Value;
+
 import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+
+// import java.util.stream.Stream;
+// import org.springframework.core.convert.converter.Converter;
+// import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+// import org.springframework.security.core.GrantedAuthority;
+// import org.springframework.security.core.authority.SimpleGrantedAuthority;
+// import org.springframework.security.oauth2.jwt.Jwt;
+// import org.springframework.security.authentication.AbstractAuthenticationToken;
 
 @Configuration
 @EnableWebSecurity
@@ -34,45 +44,57 @@ public class SecurityConfig {
      * JWT Decoder
      */
 
-//    interface AuthoritiesConverter extends Converter<Map<String, Object>, Collection<GrantedAuthority>> { }
-//
-//    @Bean
-//    AuthoritiesConverter realmRolesAuthoritiesConverter() {
-//        return claims -> {
-//            final var realmAccess = Optional.ofNullable((Map<String, Object>) claims.get("realm_access"));
-//            final var roles = realmAccess.flatMap(map -> Optional.ofNullable((List<String>) map.get("roles")));
-//            return roles.map(List::stream).orElse(Stream.empty()).map(SimpleGrantedAuthority::new).map(GrantedAuthority.class::cast).toList();
-//        };
-//    }
-//
-//    @Bean
-//    JwtAuthenticationConverter authenticationConverter(AuthoritiesConverter  authoritiesConverter) {
-//        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-//        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> authoritiesConverter.convert(jwt.getClaims()));
-//        return jwtAuthenticationConverter;
-//    }
-//
-//    @Bean
-//    SecurityFilterChain securityFilterChain(HttpSecurity http, Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter) throws Exception {
-//        http.oauth2ResourceServer(resourceServer -> {
-//            resourceServer.jwt(jwtDecoder -> {
-//                jwtDecoder.jwtAuthenticationConverter(jwtAuthenticationConverter);
-//            });
-//        });
-//
-//        http.sessionManagement(sessions -> {
-//            sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//        }).csrf(csrf -> {
-//            csrf.disable();
-//        });
-//
-//        // permit every request, block on specific controller
-//        http.authorizeHttpRequests(requests -> {
-//            requests.anyRequest().permitAll();
-//        });
-//
-//        return http.build();
-//    }
+    // interface AuthoritiesConverter extends Converter<Map<String, Object>,
+    // Collection<GrantedAuthority>> {
+    // }
+
+    // @Bean
+    // AuthoritiesConverter realmRolesAuthoritiesConverter() {
+    // return claims -> {
+    // final var realmAccess = Optional.ofNullable((Map<String, Object>)
+    // claims.get("realm_access"));
+    // final var roles = realmAccess.flatMap(map ->
+    // Optional.ofNullable((List<String>) map.get("roles")));
+    // return
+    // roles.map(List::stream).orElse(Stream.empty()).map(SimpleGrantedAuthority::new)
+    // .map(GrantedAuthority.class::cast).toList();
+    // };
+    // }
+
+    // @Bean
+    // JwtAuthenticationConverter authenticationConverter(AuthoritiesConverter
+    // authoritiesConverter) {
+    // JwtAuthenticationConverter jwtAuthenticationConverter = new
+    // JwtAuthenticationConverter();
+    // jwtAuthenticationConverter
+    // .setJwtGrantedAuthoritiesConverter(jwt ->
+    // authoritiesConverter.convert(jwt.getClaims()));
+    // return jwtAuthenticationConverter;
+    // }
+
+    // @Bean
+    // SecurityFilterChain securityFilterChain(HttpSecurity http,
+    // Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter)
+    // throws Exception {
+    // http.oauth2ResourceServer(resourceServer -> {
+    // resourceServer.jwt(jwtDecoder -> {
+    // jwtDecoder.jwtAuthenticationConverter(jwtAuthenticationConverter);
+    // });
+    // });
+
+    // http.sessionManagement(sessions -> {
+    // sessions.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    // }).csrf(csrf -> {
+    // csrf.disable();
+    // });
+
+    // // permit every request, block on specific controller
+    // http.authorizeHttpRequests(requests -> {
+    // requests.anyRequest().permitAll();
+    // });
+
+    // return http.build();
+    // }
 
     /**
      *
@@ -91,7 +113,8 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        OpaqueTokenIntrospector introspector = new KeyCloakOpaqueTokenIntrospector(introspectionUri, clientId, clientSecret);
+        OpaqueTokenIntrospector introspector = new KeyCloakOpaqueTokenIntrospector(introspectionUri, clientId,
+                clientSecret);
 
         http.oauth2ResourceServer(oauth2 -> oauth2
                 .opaqueToken(opaqueToken -> opaqueToken
